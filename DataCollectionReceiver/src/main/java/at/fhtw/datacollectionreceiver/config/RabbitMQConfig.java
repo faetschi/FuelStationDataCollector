63a7cbad2f4faae2cb38de1f9ba254ca88d659ea
Fabian Jelinek <jeli2k@users.noreply.github.com>
2024-06-15 13:22:18 +0200

    added DataCollectionReceiver

--- DataCollectionReceiver/src/main/java/at/fhtw/datacollectionreceiver/config/RabbitMQConfig.java
+++ DataCollectionReceiver/src/main/java/at/fhtw/datacollectionreceiver/config/RabbitMQConfig.java
@@ -1,4 +1,4 @@
-package at.fhtw.stationdatacollector.config;
+package at.fhtw.datacollectionreceiver.config;
 
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
@@ -7,17 +7,24 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMQConfig {
-    public static final String STATION_DATA_COLLECTOR_QUEUE = "station-data-collector";
+
+    public static final String DATA_COLLECTION_RECEIVER_QUEUE = "data-collection-receiver";
     public static final String SPECIFIC_DATA_COLLECTION_RECEIVER_QUEUE = "specific-data-collection-receiver";
+    public static final String PDF_GENERATOR_QUEUE = "pdf-generator";
 
     @Bean
-    public Queue stationDataCollectorQueue() {
-        return new Queue(STATION_DATA_COLLECTOR_QUEUE, false);
+    public Queue dataCollectionReceiverQueue() {
+        return new Queue(DATA_COLLECTION_RECEIVER_QUEUE, false);
     }
 
     @Bean
     public Queue specificDataCollectionReceiverQueue() {
         return new Queue(SPECIFIC_DATA_COLLECTION_RECEIVER_QUEUE, false);
+    }
+
+    @Bean
+    public Queue pdfGeneratorQueue() {
+        return new Queue(PDF_GENERATOR_QUEUE, false);
     }
 
     @Bean
